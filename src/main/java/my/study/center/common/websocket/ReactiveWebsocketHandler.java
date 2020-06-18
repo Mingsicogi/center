0729a86e90ecffdf65aa7f8ec1237a4197e4c517
Mingsicogi <codedoctor119@gmail.com>
2020-06-19 00:27:58 +0900

    + refactoring

--- src/main/java/my/study/center/common/websocket/ReactiveWebsocketHandler.java
+++ src/main/java/my/study/center/common/websocket/ReactiveWebsocketHandler.java
@@ -1,7 +1,7 @@
 package my.study.center.common.websocket;
 
 import lombok.extern.slf4j.Slf4j;
-import my.study.center.common.websocket.dto.Message;
+import my.study.center.common.websocket.dto.ChatMessageDTO;
 import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
@@ -9,13 +9,18 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.UnicastProcessor;
 
+/**
+ *
+ *
+ * @author minssogi
+ */
 @Slf4j
 public class ReactiveWebsocketHandler implements WebSocketHandler {
 
-    private UnicastProcessor<Message> eventPublisher;
-    private Flux<Message> events;
+    private UnicastProcessor<ChatMessageDTO> eventPublisher;
+    private Flux<ChatMessageDTO> events;
 
-    public ReactiveWebsocketHandler(UnicastProcessor<Message> eventPublisher, Flux<Message> events) {
+    public ReactiveWebsocketHandler(UnicastProcessor<ChatMessageDTO> eventPublisher, Flux<ChatMessageDTO> events) {
         this.eventPublisher = eventPublisher;
         this.events = events;
     }
@@ -25,7 +30,7 @@ public class ReactiveWebsocketHandler implements WebSocketHandler {
         ReactiveWebsocketSubscriber subscriber = new ReactiveWebsocketSubscriber(eventPublisher);
         webSocketSession.receive()
                 .map(WebSocketMessage::getPayloadAsText)
-                .map(Message::toMessage)
+                .map(ChatMessageDTO::toMessage)
                 .subscribe(subscriber::onNext, subscriber::onError, subscriber::onComplete);
 
 
