76f29baa04832272548785426b4d4f42300ef9c9
Mingsicogi <codedoctor119@gmail.com>
2020-06-18 23:51:48 +0900

    + refactoring

--- src/main/java/my/study/center/common/websocket/ReactiveWebsocketHandler.java
+++ src/main/java/my/study/center/common/websocket/ReactiveWebsocketHandler.java
@@ -1,7 +1,5 @@
 package my.study.center.common.websocket;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import my.study.center.common.websocket.dto.Message;
 import org.springframework.web.reactive.socket.WebSocketHandler;
@@ -14,7 +12,6 @@ import reactor.core.publisher.UnicastProcessor;
 @Slf4j
 public class ReactiveWebsocketHandler implements WebSocketHandler {
 
-    private static ObjectMapper objectMapper = new ObjectMapper();
     private UnicastProcessor<Message> eventPublisher;
     private Flux<Message> events;
 
@@ -28,20 +25,10 @@ public class ReactiveWebsocketHandler implements WebSocketHandler {
         ReactiveWebsocketSubscriber subscriber = new ReactiveWebsocketSubscriber(eventPublisher);
         webSocketSession.receive()
                 .map(WebSocketMessage::getPayloadAsText)
-                .map(this::toMessage)
+                .map(Message::toMessage)
                 .subscribe(subscriber::onNext, subscriber::onError, subscriber::onComplete);
 
 
         return webSocketSession.send(events.map((value) -> webSocketSession.textMessage(value.getData())));
-    }
-
-    private Message toMessage(String strMessage) {
-        try {
-            return objectMapper.readValue(strMessage, Message.class);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-
-        return null;
     }
 }
