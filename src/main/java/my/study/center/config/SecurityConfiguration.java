d5bae06731b4f4a7963a22c5143015527c28ee36
Mingsicogi <codedoctor119@gmail.com>
2020-06-28 18:22:25 +0900

    + add redis function for session management

---
+++ src/main/java/my/study/center/config/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package my.study.center.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+/**
+ *
+ */
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange().anyExchange().permitAll();
+
+        return http.build();
+    }
+}
